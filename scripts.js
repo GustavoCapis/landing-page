370c3a39997f0efe2bd596c9b29ffa91fc49bf90
Gustavo Nepomuceno Capistrano <gustavo.capy@gmail.com>
2025-11-17 19:54:06 -0300

    minor fixes on mobile sidebar menu

--- scripts.js
+++ scripts.js
@@ -1,13 +1,15 @@
 const menuButton = document.querySelector("#menu-toggle");
 const sidebar = document.querySelector("#sidebar");
 const menu = document.querySelector("#menu");
+const navLinks = document.querySelector("#nav-links");
 
 
 
 // show/hide menu on click
 menuButton.onclick = function(){
     sidebar.classList.toggle("open");
-    console.log("clicou")
+    navLinks.classList.toggle("show");
+    console.log("clicou");
 };
 
 
