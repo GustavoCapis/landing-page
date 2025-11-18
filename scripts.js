40851ffdfd0fe1c587f66ab02e133ff03ebad530
Gustavo Nepomuceno Capistrano <gustavo.capy@gmail.com>
2025-11-18 16:56:39 -0300

    change ids to classes on styles.css to prevent overrides

--- scripts.js
+++ scripts.js
@@ -1,15 +1,11 @@
-const menuButton = document.querySelector("#menu-toggle");
-const sidebar = document.querySelector("#sidebar");
-const menu = document.querySelector("#menu");
-const navLinks = document.querySelector("#nav-links");
-
-
+const menuButton = document.querySelector(".menu-toggle");
+const sidebar = document.querySelector(".sidebar");
+const menu = document.querySelector(".menu");
+const navLinks = document.querySelector(".nav-links");
 
 // show/hide menu on click
-menuButton.onclick = function(){
-    sidebar.classList.toggle("open");
-    navLinks.classList.toggle("show");
-    console.log("clicou");
+menuButton.onclick = function () {
+  sidebar.classList.toggle("open");
+  navLinks.classList.toggle("show");
+  console.log("clicou");
 };
-
-
